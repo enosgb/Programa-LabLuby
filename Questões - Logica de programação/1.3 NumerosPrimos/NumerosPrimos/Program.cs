35ba980276a7db26a922b5c9beb4353a98553ed2
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2021-07-04 17:31:06 -0300

    Add files via upload

---
+++ Questões - Logica de programação/1.3 NumerosPrimos/NumerosPrimos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumerosPrimos
+{
+    class Primos
+    {
+
+        public static int ContarNumerosPrimos(int num)
+        {
+            int Contador = 0;
+            int Primos = 0; 
+
+            for(int i = 2; i <= num; i++){
+                for (int j = 1; j <= i; j++){
+                    if (i % j == 0){
+                        Contador += 1;
+                    }
+                }
+            if (Contador <= 2){
+                    Primos += 1;
+            }
+            Contador = 0;  
+            }
+            Console.WriteLine(Primos);
+            return Primos;
+        }
+
+        static void Main(string[] args)
+        {
+        ContarNumerosPrimos(10);
+        }
+    }
+}
