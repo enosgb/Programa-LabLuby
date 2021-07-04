16dc57fb3b20fa03c3f2af7902d5f78762772d0a
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2021-07-04 17:33:09 -0300

    Add files via upload

---
+++ Questões - Logica de programação/1.7 ElementosPares/ObterElementosPares/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObterElementosPares
+{
+    class ElementosPares
+    {
+        public static int[] ObterElementosPares(int[] Vetor)
+        {
+            //int[] temp = new int[] { };
+            List<int> VetorPar = new List<int>();
+            for(int i = 0; i < Vetor.Length; i++) {
+                if(Vetor[i] % 2 == 0){
+                    VetorPar.Add(Vetor[i]);                    
+                }
+            }
+
+            int[] Vet = new int[VetorPar.Count];
+            
+            for(int i = 0; i < Vet.Length; i++){
+                Vet[i] = VetorPar[i];
+            }
+            Console.WriteLine(String.Join(",",Vet));
+            return Vet;
+        }
+        static void Main(string[] args)
+        {
+            int[] Vetor = new int[] {1,2,3,4,5};
+            ObterElementosPares(Vetor); 
+        }
+    }
+}
