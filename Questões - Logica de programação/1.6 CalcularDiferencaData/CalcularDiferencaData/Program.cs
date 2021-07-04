bc7eb748713d219eb68fb08770fddd8d8275f90c
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2021-07-04 17:32:34 -0300

    Add files via upload

---
+++ Questões - Logica de programação/1.6 CalcularDiferencaData/CalcularDiferencaData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalcularDiferencaData
+{
+    class DiferencaData
+    {
+        public static int CalcularDiferencaData(String Data1,String Data2)
+        {
+            int anos = (Convert.ToInt32(Data2.Substring(4, 4)) - Convert.ToInt32(Data1.Substring(4, 4)))*365;
+            int meses = (Convert.ToInt32(Data2.Substring(2, 2)) - Convert.ToInt32(Data1.Substring(2, 2)))*30;
+            int dias = Convert.ToInt32(Data2.Substring(0, 2))- Convert.ToInt32(Data1.Substring(0, 2));
+            dias = anos + meses + dias;
+
+            if(dias < 0){
+                dias = dias * -1;
+            }
+            Console.WriteLine(dias);
+            return dias;
+        }
+        static void Main(string[] args)
+        {
+
+            CalcularDiferencaData("10122020", "25122020"); 
+        }
+    }
+}
