b5de456e908549fde180a5263d8f48d2d5447874
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2021-07-04 17:30:01 -0300

    Add files via upload

---
+++ Questões - Logica de programação/1.1 CalcularFatorial/CalcularFatorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalcularFatorial
+{
+    class Fatorial
+    {
+        //Forma recursiva
+        public static int CalcularFatorial(int num)
+            {
+                int Fat = 0;
+                
+            if(num == 1)
+                {
+                    Fat = 1;
+                }else{
+                    Fat = num * CalcularFatorial(num - 1);
+                }
+
+            Console.WriteLine(Fat);
+            return Fat;
+            }
+
+        public static void Main(string[] args)
+        {
+            CalcularFatorial(5);
+        }
+    }
+}
