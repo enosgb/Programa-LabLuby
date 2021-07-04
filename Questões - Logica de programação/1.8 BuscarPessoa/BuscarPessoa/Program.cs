37eb91bec294204c83fb54ff215d73fc03548c79
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2021-07-04 17:33:58 -0300

    Add files via upload

---
+++ Questões - Logica de programação/1.8 BuscarPessoa/BuscarPessoa/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BuscarPessoas
+{
+    class Bpessoas
+    {
+        public static void BuscarPessoa(string[] Vetor,String Parametro)
+        {
+           
+            foreach(String Nomes in Vetor)
+            {
+                if (Nomes.Contains(Parametro))
+                {
+                    Console.Write("[{0}] ",Nomes);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] Vetor = new string[] {
+                "John Doe",
+                "Jane Doe",
+                "Alice Jones",
+                "Bobby Louis",
+                "Lisa Romero"
+                };
+            
+            BuscarPessoa(Vetor, "Doe");
+            BuscarPessoa(Vetor, "Alice");
+            BuscarPessoa(Vetor, "James");
+        }
+    }
+}
