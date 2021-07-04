db72c526f225e86ab5e553994cd76dd489c3a922
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2021-07-04 17:32:07 -0300

    Add files via upload

---
+++ Questões - Logica de programação/1.5 CalcularValorComDescontoFormatado/CalcularValorComDescontoFormatado/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValorComDescontoFormatado
+{
+    class VlrComDesconto
+    {
+        public static String CalcularValorComDescontoFormatado(String Valor,String Desconto)
+        {
+            Valor = Valor.Remove(0, 3);
+            Desconto = Desconto.Remove(Desconto.Length - 1);
+            double PorcentagemDesconto = Convert.ToDouble(Desconto)/100;
+            double Total = Convert.ToDouble(Valor)-(Convert.ToDouble(Valor) * PorcentagemDesconto);
+            String ValorComDesconto = Total.ToString("C");
+            Console.WriteLine(ValorComDesconto);
+            return ValorComDesconto;
+        }
+        static void Main(string[] args)
+        {
+
+            CalcularValorComDescontoFormatado("R$ 6.800,00", "30%");   
+        }
+    }
+}
