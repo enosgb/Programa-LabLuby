97cdf608967114abdf9ac603f976c71ed29a9c81
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2021-07-04 17:31:39 -0300

    Add files via upload

---
+++ Questões - Logica de programação/1.4 CalcularVogais/CalcularVogais/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CalcularVogais
+{
+    class Vogais
+    {
+
+        public static int CalcularVogais(String palavra)
+        {
+            int contador = 0;
+            foreach(char v in palavra)
+            {
+                switch(v)
+                {
+                    case 'a': case 'A':
+                    contador += 1;
+                    break;
+
+                    case 'e': case 'E':
+                    contador += 1;
+                    break;
+
+                    case 'i': case 'I':
+                    contador += 1;
+                    break;
+
+                    case 'o': case 'O':
+                    contador += 1;
+                    break;
+
+                    case 'u': case 'U':
+                        contador += 1;
+                    break;
+                }
+            }
+            Console.Write(contador);
+            return contador;
+        }
+        static void Main(string[] args)
+        {
+            CalcularVogais("Luby Software");
+        }
+    }
+}
