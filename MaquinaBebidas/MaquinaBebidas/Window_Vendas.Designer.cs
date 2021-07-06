6f6e49a92dfe265774ba01ee81bb017d9b2fc30e
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2021-07-06 13:55:51 -0300

    Add files via upload

---
+++ MaquinaBebidas/MaquinaBebidas/Window_Vendas.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace MaquinaBebidas
+{
+    partial class Window_Vendas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_Total_Vendas = new System.Windows.Forms.Label();
+            this.btn_Fechar_Lista_Vendas = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_Numero_Vendas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 283);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor Total:";
+            // 
+            // lb_Total_Vendas
+            // 
+            this.lb_Total_Vendas.AutoSize = true;
+            this.lb_Total_Vendas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Total_Vendas.ForeColor = System.Drawing.Color.Olive;
+            this.lb_Total_Vendas.Location = new System.Drawing.Point(185, 283);
+            this.lb_Total_Vendas.Name = "lb_Total_Vendas";
+            this.lb_Total_Vendas.Size = new System.Drawing.Size(85, 25);
+            this.lb_Total_Vendas.TabIndex = 1;
+            this.lb_Total_Vendas.Text = "R$: 0,00";
+            // 
+            // btn_Fechar_Lista_Vendas
+            // 
+            this.btn_Fechar_Lista_Vendas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Fechar_Lista_Vendas.Location = new System.Drawing.Point(17, 311);
+            this.btn_Fechar_Lista_Vendas.Name = "btn_Fechar_Lista_Vendas";
+            this.btn_Fechar_Lista_Vendas.Size = new System.Drawing.Size(253, 30);
+            this.btn_Fechar_Lista_Vendas.TabIndex = 2;
+            this.btn_Fechar_Lista_Vendas.Text = "Fechar";
+            this.btn_Fechar_Lista_Vendas.UseVisualStyleBackColor = true;
+            this.btn_Fechar_Lista_Vendas.Click += new System.EventHandler(this.btn_Fechar_Lista_Vendas_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "N° de Vendas:";
+            // 
+            // lb_Numero_Vendas
+            // 
+            this.lb_Numero_Vendas.AutoSize = true;
+            this.lb_Numero_Vendas.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Numero_Vendas.Location = new System.Drawing.Point(223, 261);
+            this.lb_Numero_Vendas.Name = "lb_Numero_Vendas";
+            this.lb_Numero_Vendas.Size = new System.Drawing.Size(20, 22);
+            this.lb_Numero_Vendas.TabIndex = 4;
+            this.lb_Numero_Vendas.Text = "0";
+            // 
+            // Window_Vendas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 450);
+            this.Controls.Add(this.lb_Numero_Vendas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Fechar_Lista_Vendas);
+            this.Controls.Add(this.lb_Total_Vendas);
+            this.Controls.Add(this.label1);
+            this.Name = "Window_Vendas";
+            this.Text = "Vendas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_Total_Vendas;
+        private System.Windows.Forms.Button btn_Fechar_Lista_Vendas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_Numero_Vendas;
+    }
+}
---
+++ MaquinaBebidas/MaquinaBebidas/Window_maquina_bebidas.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaquinaBebidas
+{
+    public partial class Window_maquina_bebidas : Form
+    {
+        static List<Bebida> bebidas;
+        static List<Venda> vendas;
+        static int VendaId = 0;
+        static double Dinheiro;
+        static double Troco;
+       
+        public Window_maquina_bebidas()
+        {
+            
+            vendas = new List<Venda>();
+            bebidas = new List<Bebida>();
+
+            //Criando as bebidas
+            bebidas.Add(new Bebida("Água", 1.50, 0));
+            bebidas.Add(new Bebida("Coca-Cola", 3.50, 0));
+            bebidas.Add(new Bebida("Guaraná", 2.75, 0));
+            bebidas.Add(new Bebida("Pepsi", 3.00, 0));
+            bebidas.Add(new Bebida("Soda", 2.25, 0));
+
+            InitializeComponent();
+        }
+
+        public void RealizarVenda(string bebida,Label labelEstoque) 
+        {
+            //Função para realizar Venda
+            bool ComEstoque = false;
+            bool ComDinheiro = false;
+            foreach (Bebida b in bebidas)
+            {
+                if (b.Descricao == bebida && b.Estoque > 0)
+                {
+                    ComEstoque = true;
+                }
+                if (b.Descricao == bebida && Dinheiro >= b.Valor)
+                {
+                    ComDinheiro = true;
+                }
+                if (b.Descricao == bebida && ComDinheiro && ComEstoque)
+                {
+                    b.Estoque = b.Estoque - 1;
+                    Troco = Dinheiro - b.Valor;
+                    Dinheiro = Dinheiro - b.Valor;
+                    labelEstoque.Text = b.Estoque.ToString();
+                    lb_Troco.Text = Troco.ToString("C");
+                    VendaId += 1;
+                    vendas.Add(new Venda(VendaId, b.Descricao, b.Valor));
+                    Variaveis_Globais.Instance.ListaVendas = vendas;
+                }
+                if (!ComDinheiro && b.Descricao == bebida)
+                {
+                    double Faltante = b.Valor - Dinheiro;
+                    lb_Dinheiro.Text = Dinheiro.ToString("C");
+                    lb_Troco.Text = "R$ 0,00";
+                    MessageBox.Show("Saldo Insuficiente! Insira:" + Faltante.ToString("C"), "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            if (!ComEstoque)
+            {
+                MessageBox.Show("Estoque indisponivel!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void btn_Zerar_Estoque_Click(object sender, EventArgs e)
+        {
+            //Zera Estoque
+            foreach (Bebida b in bebidas)
+            {
+                b.Estoque = 0;
+                if (b.Descricao == "Água") lb_Estoque_Agua.Text = (b.Estoque).ToString();
+                if (b.Descricao == "Coca-Cola") lb_Estoque_CocaCola.Text = (b.Estoque).ToString();
+                if (b.Descricao == "Guaraná") lb_Estoque_Guarana.Text = (b.Estoque).ToString();
+                if (b.Descricao == "Pepsi") lb_Estoque_Pepsi.Text = (b.Estoque).ToString();
+                if (b.Descricao == "Soda") lb_Estoque_Soda.Text = (b.Estoque).ToString();
+            }
+        }
+
+        private void btb_Abastecer_Estoque_Click(object sender, EventArgs e)
+        {
+            //Abastecer Estoque
+            foreach (Bebida b in bebidas)
+            {
+                b.Estoque = b.Estoque + 10;
+                if (b.Descricao == "Água") lb_Estoque_Agua.Text = (b.Estoque).ToString();
+                if (b.Descricao == "Coca-Cola") lb_Estoque_CocaCola.Text = (b.Estoque).ToString();
+                if (b.Descricao == "Guaraná") lb_Estoque_Guarana.Text = (b.Estoque).ToString();
+                if (b.Descricao == "Pepsi") lb_Estoque_Pepsi.Text = (b.Estoque).ToString();
+                if (b.Descricao == "Soda") lb_Estoque_Soda.Text = (b.Estoque).ToString();
+            }
+
+        }
+
+        private void btn_Zerar_Click(object sender, EventArgs e)
+        {
+            //OU btn Nova Venda
+            Dinheiro = 0;
+            Troco = 0;
+            lb_Dinheiro.Text = "R$ 0,00";
+            lb_Troco.Text = "R$ 0,00";
+        }
+
+        private void btn_VinteCincoCentavos_Click(object sender, EventArgs e)
+        {
+            Dinheiro += 0.25;
+            lb_Dinheiro.Text = (Dinheiro).ToString("C");
+        }
+
+        private void btn_CinquentaCentavos_Click(object sender, EventArgs e)
+        {
+            Dinheiro += 0.50;
+            lb_Dinheiro.Text = (Dinheiro).ToString("C");
+        }
+
+        private void btn_UmReal_Click(object sender, EventArgs e)
+        {
+            Dinheiro += 1.00;
+            lb_Dinheiro.Text = (Dinheiro).ToString("C");
+        }
+
+        private void btn_Comprar_Agua_Click(object sender, EventArgs e)
+        {
+            RealizarVenda("Água",lb_Estoque_Agua);
+        }
+
+        private void btn_Comprar_Guarana_Click(object sender, EventArgs e)
+        {
+           RealizarVenda("Guaraná",lb_Estoque_Guarana);
+        }
+        private void btn_Comprar_Pepsi_Click(object sender, EventArgs e)
+        {
+            RealizarVenda("Pepsi",lb_Estoque_Pepsi);
+        }
+
+        private void btn_Comprar_Soda_Click(object sender, EventArgs e)
+        {
+            RealizarVenda("Soda",lb_Estoque_Soda);
+        }
+
+        private void btn_Comprar_CocaCola_Click(object sender, EventArgs e)
+        {
+            RealizarVenda("Coca-Cola",lb_Estoque_CocaCola);
+        }
+
+        private void btn_Vendas_Realizadas_Click(object sender, EventArgs e)
+        {
+            if (vendas.Count > 0){
+                Window_Vendas Wvenda = new Window_Vendas();
+                Wvenda.Show();
+            }
+            else{
+                MessageBox.Show("Nenhma Venda Realizada", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+    }
+}
