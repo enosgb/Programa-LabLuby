295aa60911f3cd8019ebaba8c385541e07e8f5d8
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2021-07-06 14:12:14 -0300

    Add files via upload

---
+++ MaquinaBebidas/MaquinaBebidas/Venda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Venda
+{
+    public Venda() { }
+
+    public int VendaId { get; set; }
+    public string VendaItem { get; set; }
+    public double VendaValor { get; set; }
+
+    public Venda(int VendaId, string VendaItem, double VendaValor)
+    {
+        this.VendaId = VendaId;
+        this.VendaItem = VendaItem;
+        this.VendaValor = VendaValor;
+    }
+}
